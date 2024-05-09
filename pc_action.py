1795ed074bf68969cfb4b7ed0349eaa82874f2e1
IM94 <gladiator.pw@gmail.com>
2024-05-09 16:07:41 +0300

    (temporary) Move all steps in pc_action.py

--- pc_action.py
+++ pc_action.py
@@ -1,18 +1,74 @@
+import time
+import cv2
 import pyautogui
+import win32api
 
 
-class PcActions:
+screen_width = win32api.GetSystemMetrics(0)
+screen_height = win32api.GetSystemMetrics(1)
+
+
+class PyAutoGUIController:
 
     def __init__(self):
-        self.x = 0
-        self.y = 0
+        pass
 
-    def update_position(self, x, y) -> None:
-        self.x = x
-        self.y = y
+    def move_mouse(self, x, y):
+        # screen_width = win32api.GetSystemMetrics(0)
+        # screen_height = win32api.GetSystemMetrics(1)
+        pyautogui.moveTo((screen_width - x), y, duration=0.25)
 
-    def moving_cursor(self):
-        print("Face position (x, y) relative to screen:", self.x, self.y)
 
-        # Moving the mouse according to the coordinates of the face
-        pyautogui.moveTo(self.x, self.y, duration=0.25)
+class FaceRecognition:
+
+    def __init__(self, cascade_path, gui_controller):
+        self.cascade_path = cascade_path
+        self.clf = cv2.CascadeClassifier(self.cascade_path)
+        self.camera = cv2.VideoCapture(0)
+        self.update_interval = 1
+        self.last_update_time = time.time()
+        self.gui_controller = gui_controller
+
+    def detect_faces(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.clf.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+        return faces
+
+    def process_faces(self, faces, frame):
+        current_time = time.time()
+        for (x, y, width, height) in faces:
+            cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 255, 0), 2)
+            x_screen = int((x + (width / 2)) * screen_width / frame.shape[1])
+            y_screen = int((y + (height / 2)) * screen_height / frame.shape[0])
+            if current_time - self.last_update_time >= self.update_interval:
+                self.last_update_time = current_time
+                print("Face position (x, y) relative to screen:", x_screen, y_screen)
+                self.gui_controller.move_mouse(x_screen, y_screen)
+
+    def run(self):
+        while True:
+            _, frame = self.camera.read()
+            if frame is None:
+                break
+            faces = self.detect_faces(frame)
+            self.process_faces(faces, frame)
+            cv2.imshow('Faces', frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        self.camera.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+
+    cascade_path = './filters/haarcascade_frontalface_default.xml'
+    gui_controller = PyAutoGUIController()
+    face_recognition = FaceRecognition(cascade_path, gui_controller)
+    face_recognition.run()
