28b4d5e43c80198bd12f1cb321626336597ff438
Ivan Menkov <gladiator.pw@gmail.com>
2024-05-19 00:53:09 +0300

    Improved logic to updating coordinates, recover click method

--- hand_controls_cursor.py
+++ hand_controls_cursor.py
@@ -18,15 +18,7 @@ class HandControlsCursor:
         self.old_y_screen = 1000
 
     def get_differences(self, new_position):
-
-        x_new_position = new_position[1]
-        y_new_position = new_position[2]
-
-        # x_differences =
-        # y_differences =
-        #
-        # x_old_position =
-        # y_old_position =
+        pass
 
     def run(self):
 
@@ -41,50 +33,58 @@ class HandControlsCursor:
             find_position = self.detector.find_position(img)
 
             if len(find_position) != 0:
-                # print(f"find_position: {find_position[4]}")
-                length, _, _ = self.detector.find_distance(8, 12, img, draw=False)
+
+                # length, _, _ = self.detector.find_distance(8, 12, img, draw=False)
+                length, one, two = self.detector.find_distance(8, 12, img, draw=False)
+                length2, one2, two2 = self.detector.find_distance(4, 8, img, draw=False)
+
+                finger_tip = find_position[4]
+                screen_width = GetSystemMetrics(0)
+                screen_height = GetSystemMetrics(1)
+
+                x_screen = screen_width - (finger_tip[1] * screen_width / img.shape[1])
+                y_screen = finger_tip[2] * screen_height / img.shape[0]
+
+                if length2 < 20:
+                    self.gui_controller.click()
+                    # print("Click")
 
                 if length < 20:
                     # self.gui_controller.click()
                     # print("Click")
 
-                    finger_tip = find_position[4]
-                    screen_width = GetSystemMetrics(0)
-                    screen_height = GetSystemMetrics(1)
-
-                    x_screen = screen_width - (finger_tip[1] * screen_width / img.shape[1])
-                    y_screen = finger_tip[2] * screen_height / img.shape[0]
-
-                    # print(f"x:{x_screen}, y:{y_screen}")
-
-                    if abs(self.old_x_screen - x_screen) > 15:
+                    if abs(self.old_x_screen - x_screen) > 10:
 
                         if self.old_x_screen < x_screen:
-                            self.x_screen += 30
+                            # self.x_screen += 30
+                            self.x_screen += abs(self.old_x_screen - x_screen)
                         if self.old_x_screen > x_screen:
-                            self.x_screen -= 30
+                            # self.x_screen -= 30
+                            self.x_screen -= abs(self.old_x_screen - x_screen)
 
-                    if abs(self.old_y_screen - y_screen) > 15:
+                    if abs(self.old_y_screen - y_screen) > 10:
 
                         if self.old_y_screen < y_screen:
-                            self.y_screen += 30
+                            # self.y_screen += 30
+                            self.y_screen += abs(self.old_y_screen - y_screen)
                         if self.old_y_screen > y_screen:
-                            self.y_screen -= 30
+                            # self.y_screen -= 30
+                            self.y_screen -= abs(self.old_y_screen - y_screen)
 
                     if self.x_screen > 1920:
-                        self.x_screen = 1920
+                        self.x_screen = 1919
                     if self.x_screen <= 0:
-                        self.x_screen = 0
+                        self.x_screen = 1
 
                     if self.y_screen > 1080:
-                        self.y_screen = 1080
+                        self.y_screen = 1079
                     if self.y_screen <= 0:
-                        self.y_screen = 0
+                        self.y_screen = 1
 
                     self.gui_controller.move_mouse(self.x_screen, self.y_screen, screen_width)
 
-                    self.old_x_screen = x_screen
-                    self.old_y_screen = y_screen
+                self.old_x_screen = screen_width - (finger_tip[1] * screen_width / img.shape[1])
+                self.old_y_screen = finger_tip[2] * screen_height / img.shape[0]
 
 
 if __name__ == '__main__':
