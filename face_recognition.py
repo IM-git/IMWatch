1deb533b70201382c3d2057c1520ef503ca92c44
Ivan Menkov <gladiator.pw@gmail.com>
2024-09-03 00:07:17 +0300

    Добавил описание в методы

--- face_recognition.py
+++ face_recognition.py
@@ -2,9 +2,18 @@ import cv2
 
 
 class FaceRecognition:
+    """
+    Класс для распознавания лиц с использованием каскадов Haar и захвата видео с камеры.
+    """
 
     def __init__(self, cascade_path, screen_width, screen_height):
+        """
+        Инициализирует объект FaceRecognition.
 
+        :param cascade_path: Путь к XML-файлу каскада Хаара для распознавания лиц.
+        :param screen_width: Ширина экрана, на котором будут отображаться координаты.
+        :param screen_height: Высота экрана, на котором будут отображаться координаты.
+        """
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.clf = cv2.CascadeClassifier(cascade_path)
@@ -16,19 +25,30 @@ class FaceRecognition:
             raise Exception("Camera could not be opened.")
 
     def detect_faces(self):
+        """
+        Захватывает кадр с камеры и выполняет распознавание лиц на этом кадре.
 
+        :return: Список координат обнаруженных лиц в формате (x, y, width, height).
+        """
         _, frame = self.camera.read()
         self.frame = frame
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = self.clf.detectMultiScale(gray,
-                                          scaleFactor=1.1,
-                                          minNeighbors=5,
-                                          minSize=(30, 30),
-                                          flags=cv2.CASCADE_SCALE_IMAGE)
+        faces = self.clf.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
         return faces
 
     def process_faces(self, faces):
+        """
+        Обрабатывает координаты обнаруженных лиц для преобразования их в координаты экрана.
 
+        :param faces: Список координат обнаруженных лиц в формате (x, y, width, height).
+        :return: Координаты экрана (x_screen, y_screen), соответствующие центру первого обнаруженного лица.
+        """
         x_screen = 0
         y_screen = 0
 
@@ -39,5 +59,7 @@ class FaceRecognition:
         return x_screen, y_screen
 
     def release(self):
-        """ Освобождает ресурсы камеры. """
+        """
+        Освобождает ресурсы камеры.
+        """
         self.camera.release()
--- run.py
+++ run.py
@@ -1,28 +1,31 @@
-import cv2
-import pygame
-
 import time
-
+import pygame
 from face_recognition import FaceRecognition
 from ball import Ball
 
+# Путь к каскаду Хаара для распознавания лиц
 cascade_path = './filters/haarcascade_frontalface_default.xml'
 
+# Параметры экрана и интервала обновления
 screen_width = 800
 screen_height = 600
-
 update_interval = 0.1
 
+# Параметры шара и цвета экрана
 ball_radius = 20
-
-ball_color = (255, 255, 0)
-screen_color = (255, 255, 255)
+ball_color = (255, 255, 0)  # Жёлтый цвет шара
+screen_color = (255, 255, 255)  # Белый цвет фона
 
 
 def run():
+    """
+    Основная функция запуска приложения для отслеживания головы с использованием распознавания лиц.
 
+    Инициализирует экран, создает объекты для распознавания лиц и шара,
+    запускает главный цикл, который обрабатывает видео с камеры,
+    распознает лица и обновляет положение шара на экране в зависимости от положения лица.
+    """
     last_update_time = time.time()
-
     pygame.init()
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Head Tracking Ball")
@@ -30,36 +33,35 @@ def run():
     ball = Ball(ball_radius, ball_color)
     face_recognition = FaceRecognition(cascade_path, screen_width, screen_height)
 
-    while True:
+    try:
+        while True:
+            # Захват и обработка лиц
+            faces = face_recognition.detect_faces()
+            _screen_coordinates = face_recognition.process_faces(faces)
+            x_screen, y_screen = _screen_coordinates
 
-        faces = face_recognition.detect_faces()
+            current_time = time.time()
 
-        _screen_coordinates = face_recognition.process_faces(faces)
-        x_screen = _screen_coordinates[0]
-        y_screen = _screen_coordinates[1]
-
-        current_time = time.time()
-
-        if current_time - last_update_time >= update_interval:
-            last_update_time = current_time
-
+            # Обновление положения шара только при наличии обнаруженных лиц
             if x_screen is not None and y_screen is not None:
-                ball.update_position(screen_width - x_screen, y_screen)
+                if current_time - last_update_time >= update_interval:
+                    last_update_time = current_time
+                    ball.update_position(screen_width - x_screen, y_screen)
 
-            ball.update_position(screen_width - x_screen, y_screen)
+            # Очистка экрана и рисование шара
+            screen.fill(screen_color)
+            ball.draw(screen)
+            pygame.display.update()
 
-        screen.fill(screen_color)
-        ball.draw(screen)
-        pygame.display.update()
+            # Обработка событий Pygame
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                face_recognition.release()
-                pygame.quit()
-                return
-
-        if cv2.waitKey(1) == ord('q'):
-            break
+    finally:
+        # Обязательно освобождаем ресурсы камеры перед выходом
+        face_recognition.release()
 
 
 if __name__ == '__main__':
